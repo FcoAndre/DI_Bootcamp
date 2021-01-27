7e90b06ef87fb7c9ad2060c40e23f46c8b758710
FcoAndre <francisco.andre.santana@gmail.com>
2021-01-28 01:02:26 +0200

    280120

---
+++ Week4/Day2/Daily Challenge/script.js
@@ -0,0 +1,143 @@
+//  Exercise 1 : Keyless Car
+
+// var age = prompt('Qual a sua idade?');
+
+// function checkDriverAge(age) {
+//     if (age > 18) {
+//         alert("You are old enough to drive, Powering On. Enjoy the ride!")
+//     } else if (age < 18) {
+//         alert("Sorry, you are too young to drive this car. Powering off")
+//     } else if (age == 18) {
+//         alert("Congratulations on your first year of driving. Enjoy the ride!")
+//     } else {
+//         alert("I do not understand you")
+//     }
+// }
+
+// checkDriverAge(age);
+
+
+
+
+
+
+
+
+
+
+// Exercise 2 : What’s In My Wallet ?
+
+
+// const changeEnough = function (coins, price)  {
+//     let coinsTotal = [coins[0]*0.25, coins[1]*0.10, coins[2]*0.05, coins[3]*0.01];
+//         coinsTotal= [coinsTotal[0]+coinsTotal[1]+coinsTotal[2]+coinsTotal[3]];
+        
+//         if(coinsTotal>=price) {
+//             console.log("True")
+//         }
+//         else{
+//             console.log("False")
+//         }
+  
+// }
+
+// changeEnough([2, 100, 0, 0], 1004.11)
+
+
+
+
+
+
+
+
+
+
+// Exercise 3
+
+// numerototal =0
+
+// for (let numero  = 0; numero  <= 500; numero ++){
+           
+
+//      if (0 == numero % 23){
+//        console.log (numero + " " + "is a multiple"); 
+//        numerototal = numerototal +numero
+//            }
+//     }
+
+//     console.log(numerototal + ' is the total')
+
+
+
+
+
+
+
+
+
+// Exercise 4
+
+    // let amazonBasket = {
+    //     glasses: 1,
+    //     books: 2,
+    //     floss: 100
+    // }
+
+    // let obj = prompt('What do you want to add to your basket?');
+
+    // function checkBasket(obj){
+    //     if(obj == "glasses"){
+    //         if ("glasses" in amazonBasket){
+    //             alert("This item was already added to your basket")
+    //         }
+    //     }  
+    //     else if(obj == "books"){
+    //         if ("books" in amazonBasket){
+    //           alert("This item was already added to your basket")                }
+    //         }    
+    //     else if(obj == "floss"){
+    //         if ("floss" in amazonBasket){
+    //           alert("This item was already added to your basket")                    }   
+    //             }
+    //     else{
+    //         alert("This item was not added to your basket yet");
+    //     }
+    // }
+
+    //     checkBasket(obj);
+
+
+
+
+// I was not able to do Exercise 5, 6 and 7
+
+  // Exercise 5
+
+
+
+//   let shoppingList = ["banana", "orange", "apple"];
+
+//   let stock = { 
+//     "banana": 6, 
+//     "apple": 0,
+//     "pear": 12,
+//     "orange": 32,
+//     "blueberry":1
+// }  
+
+// let prices = {    
+//     "banana": 4, 
+//     "apple": 2, 
+//     "pear": 1,
+//     "orange": 1.5,
+//     "blueberry":10
+// } 
+
+
+// function myBill () {
+
+// }
+// console.log(shoppingList)
+
+
+
