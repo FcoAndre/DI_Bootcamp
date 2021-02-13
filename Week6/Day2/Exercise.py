09bf3d78ae058e9bbfe899e2c0d32b17e71e7b39
FcoAndre <francisco.andre.santana@gmail.com>
2021-02-13 17:47:48 +0200

    week601

---
+++ Week6/Day2/Exercise.py
@@ -0,0 +1,106 @@
+# # Exercise 1 : Hello World
+
+# a_string = "Hello world"
+# for line in range(4):
+#   result = a_string 
+#   print(result)
+
+
+
+# # Exercise 2 : Some Math
+
+# base = 99
+# exponent = 3
+# result = pow(base, exponent)*8
+# print(result)
+
+
+
+# # Exercise 3 : What Is The Output ?
+
+# print (5 < 3)
+
+# print(3 == 3)
+
+# print(3 == "3")
+
+# print("3" > 3) # I dont get why we get true here
+
+# print("Hello" == "hello")
+
+
+
+# Exercise 4 : Your Computer Brand
+
+# # Create a variable called computer_brand that contains the brand of your computer.
+
+# computer_brand = "apple"
+
+# # Insert and print the above variable in a sentence,like "I have a razer computer".
+
+# print "I have an", computer_brand, "computer"
+
+
+
+
+# # Exercise 5: Your Information
+# # Create a variable called name, and give it your name as a value (text)
+
+# name = "Francisco"
+
+# # Create a variable called age, and give it your age as a value (number)
+
+# age = 32
+
+# # Create a variable called shoe_size, and give it your shoe size as a value
+
+# shoe_size = 43
+
+# # Create a variable called info. Its value should be an interesting sentence about yourself,
+# #  including your name, age, and shoe size. Use the variables you created earlier.
+
+# info = "My name is", name, "I am", age, "years old and my shoe size is", shoe_size
+
+# # Have your code print the info message.
+
+# print info
+# print "My name is", name, "I am", age, "years old and my shoe size is", shoe_size
+
+# # Run your code
+
+
+# # Exercise 6 : A & B
+
+# # Given two variables a and b that you need to define, 
+# # make a program that print Hello World only if a is greater than b.
+
+# a = 8
+# b = 6
+
+# if a>b:
+#     print "Hello World"
+
+
+
+# # Exercise 7 : Odd Or Even
+# # Write a script that asks the user for a number and determines whether this number is odd or even.
+
+# numero = 6
+
+# if (numero % 2) == 0:
+#    print"Even numero"
+# else:
+#    print"Odd numero"
+
+
+# Exercise 8 : What’s Your Name ?
+
+
+# name_guest = input("Enter your name")
+
+# name_mine = "Francisco"
+
+# if name_guest == name_mine :
+#    print"How come our parents had such a bad taste for names?"
+# else:
+#    print"I am sure that you also wanted to be name Francisco"
---
+++ Week6/Day5/Exercises.py
@@ -0,0 +1,18 @@
+# # Exercise 1 : Convert Lists Into Dictionaries
+
+# keys = ['Ten', 'Twenty', 'Thirty']
+# values = [10, 20, 30]
+
+# for item in zip(keys,values):
+#     print(item)
+
+
+
+# Exercise 2 : Cinemax #2
+
+
+
+
+# Exercise 3: Zara
+
+
