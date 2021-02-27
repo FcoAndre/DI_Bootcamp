8101fb49d28596689093edc34fb14f531d90f0c9
FcoAndre <francisco.andre.santana@gmail.com>
2021-02-27 18:59:51 +0200

    week7

---
+++ Week7/Day2/Exercise/Exercise.py
@@ -0,0 +1,88 @@
+# # Exercise 1 : What Are You Learning ?
+
+
+# def display_message():
+#     return('I am learning python! =)')
+    
+# display_message()
+
+
+
+# # Exercise 2: What’s Your Favorite Book ?
+
+# def favorite_book(title) :
+    
+#     print (f"One of my favorite books is {title}”.")
+
+# favorite_book("title")
+
+
+
+# # Exercise 3 : Some Geography
+
+
+# def describe_city(city,country="Iceland") :
+    
+#     print (f"{city} is in {country}”.")
+
+# describe_city("London","UK")
+# describe_city("Reykjavik",)
+# describe_city("Rio","Brasil")
+
+
+
+
+# # Exercise 4 : Random
+
+
+# import random
+
+# def comparacao(numero):
+
+#     n = random.randint(1,100)
+
+#     if numero == n:
+#         print("You chose the same number")
+#     else:
+#         print (f"You chose a different number. The random number is {n} and you typed {numero}")
+        
+
+# comparacao(3)
+
+
+
+# # Exercise 5 : Let’s Create Some Personalized Shirts !
+
+
+# def make_shirt(size,text):
+#     print (f"You chose a shirt size {size} and - {text} - is gonna be printed on it")
+
+
+# make_shirt("M","hey you")
+
+# make_shirt(text="Drag Race",size="L")
+
+
+
+
+
+# # Exercise 6 : Magicians …
+
+
+# magicians = ["Mister M", "David Copperfield", "Ru Paul"]
+
+# def show_magicians(magics):
+#     for magic in magics:
+#         print (magic)
+
+# # show_magicians(magicians)
+
+
+# def make_great(magics):
+#     for magic in magics:
+#         magic = magic + " the great"
+#         print(magic)
+        
+
+# make_great(magicians)
+# show_magicians(magicians)
